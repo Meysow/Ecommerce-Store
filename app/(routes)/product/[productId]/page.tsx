c86b592399f237926b6511c244dcb3a35e712fc8
Thibault Dilman <thibault.dilman@protonmail.com>
2023-09-20 14:38:51 +0400

    Indivudual page Done & Starting Category

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,6 +1,8 @@
 import getProduct from '@/actions/get-product';
 import getProducts from '@/actions/get-products';
 import Gallery from '@/components/gallery';
+import Info from '@/components/info';
+import ProductList from '@/components/product-list';
 import Container from '@/components/ui/container';
 
 interface ProductPageProps {
@@ -21,13 +23,16 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
       <Container>
         <div className='px-4 py-10 sm:px-6 lg:px-8'>
           <div className='lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8'>
-            <Gallery />
-            <div>Galery</div>
+            <Gallery images={product.images} />
             <div className='mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0'>
-              {/* info */}
-              Info
+              <Info data={product} />
             </div>
           </div>
+          <hr className='my-10' />
+          <ProductList
+            title='Related Items'
+            items={suggestedProduct}
+          />
         </div>
       </Container>
     </div>
